5dc0700a4f1e0ebf7d6ceeee48cb622713657e8e
MiraOka <uaofmsbct.9g@gmail.com>
2018-10-23 13:00:11 +0900

    add microphone

---
+++ Assets/Developments/Scripts/MicManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicManager : MonoBehaviour {
+
+    AudioSource audio;
+
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 1, 44100);
+        audio.Play();
+	}
+	
+	void Update () {
+        float[] data = new float[256];
+        float vol = 0;
+        audio.GetOutputData(data, 0);
+
+        foreach(float s in data)
+        {
+            vol += Mathf.Abs(s);
+        }
+
+        Debug.Log(vol);
+	}
+}
